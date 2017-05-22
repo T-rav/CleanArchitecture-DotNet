df9695eed9cdaafe2d23e61a4a848a5c8815951b
Travis Frisinger <travis.frisinger@spar.co.za>
2017-05-22 08:47:36 +0200

    Added IUseCaseAsync and IActionAsync

---
+++ source/TddBuddy.CleanArchitecture.Domain/IActionAsync.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using TddBuddy.CleanArchitecture.Domain.Output;
+using TddBuddy.CleanArchitecture.Domain.TOs;
+
+namespace TddBuddy.CleanArchitecture.Domain
+{
+    public interface IActionAsync<out IItOutputTo>
+    {
+        Task Execute(IRespondWithSuccessOrError<IItOutputTo, ErrorOutputTo> presenter);
+    }
+}
