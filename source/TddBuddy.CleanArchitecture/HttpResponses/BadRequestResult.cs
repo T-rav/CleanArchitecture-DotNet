2fe4161da7356c4303ba3b585010626aeedfa4a0
Travis Frisinger <travis.frisinger@spar.co.za>
2017-05-22 16:13:37 +0200

    Renamed BadRequestEntityResult to BadRequestResult

---
+++ source/TddBuddy.CleanArchitecture/HttpResponses/BadRequestResult.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace TddBuddy.CleanArchitecture.HttpResponses
+{
+    public class BadRequestResult<T> : NegotiatedContentResult<T>
+    {
+        private const HttpStatusCode BadRequestHttpStatusCode = HttpStatusCode.BadRequest;
+
+        public BadRequestResult(T content, IContentNegotiator contentNegotiator, HttpRequestMessage request, IEnumerable<MediaTypeFormatter> formatters) : base(BadRequestHttpStatusCode, content, contentNegotiator, request, formatters)
+        {
+        }
+
+        public BadRequestResult(T content, ApiController controller) : base(BadRequestHttpStatusCode, content, controller)
+        {
+        }
+    }
+}
