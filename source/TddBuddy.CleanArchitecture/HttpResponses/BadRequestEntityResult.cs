e5a923d98bb00ec1223b9816d01617fe85d0a9cd
Travis Frisinger <travis.frisinger@spar.co.za>
2017-05-22 16:11:11 +0200

    Added BadRequestEntityResult.cs

---
+++ source/TddBuddy.CleanArchitecture/HttpResponses/BadRequestEntityResult.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace TddBuddy.CleanArchitecture.HttpResponses
+{
+    public class BadRequestEntityResult<T> : NegotiatedContentResult<T>
+    {
+        private const HttpStatusCode BadRequestHttpStatusCode = HttpStatusCode.BadRequest;
+
+        public BadRequestEntityResult(T content, IContentNegotiator contentNegotiator, HttpRequestMessage request, IEnumerable<MediaTypeFormatter> formatters) : base(BadRequestHttpStatusCode, content, contentNegotiator, request, formatters)
+        {
+        }
+
+        public BadRequestEntityResult(T content, ApiController controller) : base(BadRequestHttpStatusCode, content, controller)
+        {
+        }
+    }
+}
