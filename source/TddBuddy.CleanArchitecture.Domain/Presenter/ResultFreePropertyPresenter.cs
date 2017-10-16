8198cf12fe1f8fd2855ceff7e7db1af6fe0d17ca
T-rav <travis.frisinger@chillisoft.co.za>
2017-10-16 13:02:14 +0200

    Added ResultFreePropertyPresenter to help with testing IResultFreeSuccessOrError

---
+++ source/TddBuddy.CleanArchitecture.Domain/Presenter/ResultFreePropertyPresenter.cs
@@ -0,0 +1,24 @@
+﻿using TddBuddy.CleanArchitecture.Domain.Output;
+
+namespace TddBuddy.CleanArchitecture.Domain.Presenter
+{
+    public class ResultFreePropertyPresenter<TError> : IRespondWithNoResultSuccessOrError<TError>
+    {
+        public TError ErrorContent { get; private set; }
+
+        public void Respond(TError output)
+        {
+            ErrorContent = output;
+        }
+
+        public void Respond()
+        {
+
+        }
+
+        public bool IsErrorResponse()
+        {
+            return ErrorContent != null;
+        }
+    }
+}
